8a53e3b41cd3296faa1b5d49b80bfcd927400a63
Mashaun18 <animashaunoreoluwa@gmail.com>
2023-11-28 16:47:33 +0100

    fix modify Spotify file

--- Spotify/Spotify/urls.py
+++ Spotify/Spotify/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('spotify_app.urls')),
+    path('', include('spotify_app.urls')),
 ]
--- Spotify/spotify_app/models.py
+++ Spotify/spotify_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Artist(models.Model):
     name = models.CharField(max_length=255)
-    image = models.URLField()
+    image = models.ImageField(upload_to="song_images")
     bio = models.TextField()
     age = models.IntegerField()
 
